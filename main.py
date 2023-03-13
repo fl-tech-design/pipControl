c1774f61a04d0797e75d13aa1caef4589fea0978
fl-tech-design <fl-tech-design@gmx.ch>
2023-03-13 17:44:55 +0100

    add setup.py bugfixes

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import subprocess
 
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.properties import StringProperty
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
+
+
+class HyperlinkLabel(ButtonBehavior, Label):
+    url = StringProperty()
+
+    def on_release(self):
+        if self.url:
+            Window.open(self.url)
 
 
 class ScrMain(Screen):
@@ -17,15 +29,17 @@ class WindowManager(ScreenManager):
 
 
 class MainApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.li_package_name = []
+
     def build(self):
         return WindowManager()
 
-    @staticmethod
-    def check_pip_outdated():
+    def check_pip_outdated(self):
         print("check outgradet")
         command = ['pip', 'list', '--outdated']
         output = subprocess.check_output(command)
-
         if output.strip():
             with open('old_packs.txt', 'w') as f:
                 f.write(output.decode('utf-8'))
@@ -35,8 +49,17 @@ class MainApp(App):
                 f.write("Es gibt keine veralteten Pakete.")
             print("Es gibt keine veralteten Pakete.")
         print(output.decode("utf-8"))
+        self.get_outdated_package_name()
 
+    def get_outdated_package_name(self):
+        with open('outdated_packages.txt', 'r') as file:
+            self.li_package_name = [line.split()[0] for line in file.readlines()[2:]]
+        print(self.li_package_name)
+    def link_clicked(self, link):
+        if link == 'world':
+            print('World clicked')
 
 if __name__ == '__main__':
     app = MainApp()
+
     app.run()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='pipControl',
+    version='0.1',
+    author='fl-tech & design',
+    author_email='fl-tech-desig@gmx.ch',
+    description='A simple gui to control pip.',
+    url='https://github.com/fl-tech-design/pipControl.git',
+    install_requires=requirements
+)
